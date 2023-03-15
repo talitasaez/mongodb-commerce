7a236a9fa33bf808f2a813e84f876d8e5946c47f
talitasaez <tltsaez@gmail.com>
2023-03-15 17:54:14 -0300

    req 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,8 @@
+db.produtos.updateMany(
+    { nome: { $nin: ["McChicken"] } },
+     { $addToSet: { ingredientes: "Ketchup" } },
+    );
+    db.produtos.find(
+        { },
+         { _id: 0, nome: 1, ingredientes: 1 },
+        );        
