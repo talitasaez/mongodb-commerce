47b8221335001fa97ab7e73a01f0aeb7fc374945
talitasaez <tltsaez@gmail.com>
2023-03-15 18:01:26 -0300

    req 12 erro lint

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,8 +1,4 @@
-db.produtos.updateMany(
-    { nome: { $nin: ["McChicken"] } },
-     { $addToSet: { ingredientes: "Ketchup" } },
-    );
-    db.produtos.find(
-        { },
-         { _id: 0, nome: 1, ingredientes: 1 },
-        );        
+db.produtos.updateMany({ nome: { $nin: ["McChicken"] } },
+{ $addToSet: { ingredientes: "ketchup" } });
+
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
