c86538e5be42fc0373ca245bc9f1ee8e549fb67c
talitasaez <tltsaez@gmail.com>
2023-03-15 18:59:43 -0300

    req 18 erro lint

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,4 +1,10 @@
-db.produtos.updateMany({ nome: { $nin: ["Big Mac", "Quarteirão com Queijo"] } },
-{ $push: { ingredientes: "bacon" } });
-
-db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
+db.produtos.updateMany(
+    { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
+    {
+      $push: { ingredientes: "bacon" },
+    },
+  );
+  db.produtos.find(
+    {},
+    { _id: 0, nome: 1, ingredientes: 1 },
+  );
